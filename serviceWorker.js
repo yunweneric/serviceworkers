48c6a9aea9293cef267b34ca96d1212961a45b42
honorezemagho <42036185+honorezemagho@users.noreply.github.com>
2020-10-03 13:24:25 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -4,8 +4,15 @@ const assets = [
     "/index.html",
     "/css/style.css",
     "/js/app.js",
-    "/images/*",
-
+    "/images/coffee1.jpg",
+    "/images/coffee2.jpg",
+    "/images/coffee3.jpg",
+    "/images/coffee4.jpg",
+    "/images/coffee5.jpg",
+    "/images/coffee6.jpg",
+    "/images/coffee7.jpg",
+    "/images/coffee8.jpg",
+    "/images/coffee9.jpg",
 ]
 
 self.addEventListener("install", installEvent => {
